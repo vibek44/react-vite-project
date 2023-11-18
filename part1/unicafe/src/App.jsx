55ce55b8edd47b7f492ed751071ae201668a721a
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-18 20:14:50 +0200

    adding component and display data in table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,12 +2,30 @@ import React from "react";
 import {useState} from "react"
 
 const Button=({text,onClick})=><button onClick={onClick}>{text}</button>
-const Statistic=({good,neutral,bad})=><>
-      <p>good {good} </p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad} </p>
 
-     </>
+const StatisticLine=({text,value})=><tr>
+    <td>{text}</td> 
+    <td>{value}</td>
+</tr>
+
+const Statistic=({good,neutral,bad})=>{
+    let total=good+bad+neutral;
+    let average=(good-bad)/total
+    let positive=(good/total)*100+'%'
+    return(<>
+        <table>
+            <tbody>
+                <StatisticLine text='good' value={good}/>
+                <StatisticLine text='bad'  value={bad}/>
+                <StatisticLine text='neutral' value={neutral}/>
+                <StatisticLine text='all' value={total}/>
+                <StatisticLine text='average' value={average}/>
+                <StatisticLine text='positive' value={positive}/>
+            </tbody>
+        </table>
+    </>)
+}
+    
 
 const App=()=>{
     const [good,setGood]=useState(0)
@@ -19,16 +37,18 @@ const App=()=>{
     const handleNeutral=()=>setNeutral(neutral+1)
 
     const handleBad=()=>setBad(bad+1)
-    return(
-        <>
+
+    return(<>
         <h3>Give feedback</h3>
         <Button onClick={handleGood} text='good'/>
         <Button onClick={handleNeutral} text='neutral'/>
         <Button onClick={handleBad} text='bad'/>
-        <p>Statistics</p>
-        <Statistic good={good} bad={bad} neutral={neutral}/>
-        </>
-    )
+        <h3>Statistics</h3>
+        {(good+bad+neutral>0)?<Statistic good={good} bad={bad} neutral={neutral}/>:
+          <p>No feedbacks given</p>
+        }
+
+    </>)
 }
 
 
