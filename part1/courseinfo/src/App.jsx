a257cea1b40a98fddd50f6b4c53797f1b297a1d6
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-18 11:28:12 +0200

    adding components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,10 +1,37 @@
 import { useState } from 'react'
 
+const Header=({course})=><h1>{course.name}</h1>
+
+const Part=({part})=><p>{part.name} {part.exercises}</p>
+
+const Content=({course})=>course.parts.map(part=><Part key={part.exercises} part={part}/>)
+ 
+const Total=({course})=><p>Total: {course.parts.reduce((sum,nel)=>sum+nel.exercises,0)}</p>
 
 const App=()=> {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <>          
-    <p>courseinfoapp</p>
+     <Header course={course}/>
+     <Content course={course}/>
+     <Total course={course}/>
+
     </>
   )
 }
