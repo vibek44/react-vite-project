5b91bb047ee83d6e4408ebb154c6f43950e2a739
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-19 21:20:09 +0200

    creating course component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,32 +8,62 @@ const Content=({course})=>course.parts.map(part=><Part key={part.exercises} part
  
 const Total=({course})=><p>Total: {course.parts.reduce((sum,nel)=>sum+nel.exercises,0)}</p>
 
-const App=()=> {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-  return (
-    <>          
-     <Header course={course}/>
-     <Content course={course}/>
-     <Total course={course}/>
+const Course=({course})=><>          
+  <Header course={course}/>
+  <Content course={course}/>
+  <Total course={course}/>
+</>
 
-    </>
-  )
+const App=()=> {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return (
+    <>
+     {courses.map(course=><div><Course key={course.id} course={course}/></div>)}
+    </>)
+    
 }
 
 export default App
