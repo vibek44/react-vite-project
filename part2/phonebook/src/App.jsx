21bc10684f017c7838d79eaca759c1e125b06c95
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-20 17:34:23 +0200

    adding form handler

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Person from './components/person'
 
 
 const  App=()=> {
@@ -6,21 +7,38 @@ const  App=()=> {
     { name: 'Arto Hellas' }
   ])
 
+  const [name, setName]=useState('')
+
+  const handleName=(e)=>{
+    setName(e.target.value.trim())
+  }
 
   const handleSubmit=(e)=>{
     e.preventDefault()
-   console.log(e.target);
+    let result =persons.find(person=>person.name.toLowerCase()===name.toLowerCase())
+    if(result){
+       return alert(`${name} is already added in facebook`)
+    }
+    const person={name}
+    setPersons(persons.concat(person))
+    setName('')
+   
   }
 
   return (
      <div>
         <h2>Phonebook</h2> 
         <form onSubmit={handleSubmit}>
-          <input type="text"/>
-          <button type='submit'>submit</button>
+          <div>
+           <input type="text" value={name} onChange={handleName}/>
+          </div>
+          <div>
+           <button type='submit'>Add</button>
+          </div>
+          
         </form>
         <h2>Contact</h2>
-        {persons.map(person=><p key={person.name}>{person.name}</p>)}
+        {persons.map(person=><Person key={person.name} person={person}/>)}
      </div>
   )
 }
