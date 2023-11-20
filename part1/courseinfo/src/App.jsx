255ef4da0f25e5849a6b55941faf22678efcc9ac
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-20 16:23:12 +0200

    Importing couse component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -50,7 +50,7 @@ const App=()=> {
   return (
     <>
       <h1>Web development curriculum</h1>
-      {courses.map(course=>
+      { courses.map(course=>
           <Course key={course.id} course={course}/>
       
       )}
