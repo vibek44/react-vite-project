383d8dce02eb2de3d66489e66204bf2bbb0b4b6f
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-20 13:49:16 +0200

    adding content component for parts

--- part1/courseinfo/src/components/course.jsx
+++ part1/courseinfo/src/components/course.jsx
@@ -3,14 +3,18 @@
 const Header=({course})=><h1>{course.name}</h1>
 
 const Part=({part})=><p>{part.name} {part.exercises}</p>
+
+const Content=({course})=><>
+   {course.parts.map(part=><Part  key={part.id} part={part}/>)}
+</>
  
 const Total=({course})=><p>Total: {course.parts.reduce((sum,nel)=>sum+nel.exercises,0)} exercises</p>
 
 
 
 const Course=({course})=><div>          
-  <Header course={course}/>
-  {course.parts.map(part=><Part  key={part.id} part={part}/>)}
+  <Header course={course} />
+  <Content course={course} />
   <Total course={course}/>
 </div>
 
