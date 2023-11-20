a2725accc3edfe131122963a52fc38a1bfd360df
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-20 21:00:34 +0200

    adding useeffect hook to fetch data

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,21 +2,23 @@ import { useState } from 'react'
 import Filter from './components/filter'
 import ContactForm from './components/contactform'
 import Person from './components/person'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 
 const  App=()=> {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [name, setName]=useState('')
 
   const [number, setNumber]=useState('')
 
   const [search, setSearch]=useState(null)
+
+  useEffect(()=>{
+    axios.get('http://localhost:3001/persons')
+      .then(res=>setPersons(res.data))    
+  },[])
 
   const peopleToShow=search ?
   persons.filter(person=>
