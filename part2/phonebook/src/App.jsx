809c77873dba67fdfe0868cabae896f8b2dacaf5
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-20 19:46:38 +0200

    adding search field functionality

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,36 @@
 import { useState } from 'react'
+import Filter from './components/filter'
+import ContactForm from './components/contactform'
 import Person from './components/person'
 
 
 const  App=()=> {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [name, setName]=useState('')
 
+  const [number, setNumber]=useState('')
+
+  const [search, setSearch]=useState(null)
+
+  const peopleToShow=search ?
+  persons.filter(person=>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  ):persons
+
+  //const personToShow
+  const handleSearch=(e)=>setSearch(e.target.value)
+
   const handleName=(e)=>{
     setName(e.target.value)
   }
+
+  const handleNumber=(e)=>setNumber(e.target.value)
 
   const handleSubmit=(e)=>{
     e.preventDefault()
@@ -19,26 +38,25 @@ const  App=()=> {
     if(result){
        return alert(`${name} is already added in facebook`)
     }
-    const person={name:name.trim()}
+    const person={ name:name.trim(),
+      number:number.toString(),
+      id:persons.length+1
+    }
     setPersons(persons.concat(person))
     setName('')
-   
+    setNumber('')
+     
   }
 
   return (
      <div>
         <h2>Phonebook</h2> 
-        <form onSubmit={handleSubmit}>
-          <div>
-           <input type="text" onChange={handleName} value={name} />
-          </div>
-          <div>
-           <button type='submit'>Add</button>
-          </div>
-          
-        </form>
+        <Filter handleSearch={handleSearch}/>
+        <h4> Add Contacts</h4>
+        <ContactForm name={name} number={number} handleSubmit={handleSubmit} 
+         handleNumber={handleNumber} handleName={handleName} />
         <h2>Contact</h2>
-        {persons.map(person=><Person key={person.name} person={person}/>)}
+        {peopleToShow.map(person=><Person key={person.id} person={person}/>)}
      </div>
   )
 }
